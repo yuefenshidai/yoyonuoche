f9305a147bb81784dad2c441bcbddec6e8e27d48
lvshenglin <15308077613@163.com>
2018-04-04 14:07:42 +0800

    拨打电话修改

--- YOYO一键挪车/pages/working/working.js
+++ YOYO一键挪车/pages/working/working.js
@@ -69,7 +69,8 @@ Page({
 	modalClose(){
 		if(this.data.call){
 			wx.makePhoneCall({
-				phoneNumber: '02866004346 '
+				// phoneNumber: '02866004346 '
+        phoneNumber: '0289669910'
 			})
 		}
 		this.setData({
@@ -119,7 +120,7 @@ Page({
 			if(res.statusCode==200){
 				if (res.data.code==0){
 					this.setData({
-						AlertContent: '请拨打02866004346 ，系统将会为你自动接通车主',
+            AlertContent: '请拨打0289669910 ，系统将会为你自动接通车主',
 						call: true
 					})
 				}
